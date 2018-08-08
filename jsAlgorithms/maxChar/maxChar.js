24e18a257551267ed450135951b8a04b21407495
Regina Galieva <rg.galieva@gmail.com>
2018-08-08 13:12:50 -0700

    maxChar

---
+++ jsAlgorithms/maxChar/maxChar.js
@@ -0,0 +1,23 @@
+// Given a string, return the character that is most
+// commonly used in the string.
+
+const maxChar = str => {
+    let maxFrequency = 0;
+    let maxValue;
+
+    const maxCharMap = str.split('').reduce((maxCharMapTemp, char) => {
+        maxCharMapTemp[char] = maxCharMapTemp[char] + 1 || 1;
+        return maxCharMapTemp;
+    }, {});
+
+    for (let char in maxCharMap) {
+        if (maxCharMap[char] > maxFrequency) {
+            maxFrequency = maxCharMap[char];
+            maxValue = char;
+        }
+    }
+
+    return maxValue;
+}
+
+module.exports = maxChar;
---
+++ jsAlgorithms/reverseInteger/reverseInteger.js
@@ -0,0 +1,16 @@
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+const reverseInt = n => {
+    const reversedNumber =  n.toString().split('').reverse().join('');
+
+    return parseInt(reversedNumber) * Math.sign(n);
+}
+
+module.exports = reverseInt;
