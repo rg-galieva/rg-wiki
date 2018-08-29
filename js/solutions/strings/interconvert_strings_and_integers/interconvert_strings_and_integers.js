7550d7398e3e35fb47e7340b12c94cfd306eb335
Regina Galieva <rg.galieva@gmail.com>
2018-08-28 20:27:20 -0700

    interconvet description

--- js/solutions/strings/interconvert_strings_and_integers/interconvert_strings_and_integers.js
+++ js/solutions/strings/interconvert_strings_and_integers/interconvert_strings_and_integers.js
@@ -1,10 +1,11 @@
 /*
+parseInt or toString are forbidden to use
 "314" => 314
 "-314" => -314
 314 => "314"
-parseInt or toString are forbidden to use
 */
 
+// 315 = (3 * 10**3) + (1 * 10**2) + 5 * (10**1)
 function convertStringToIntegers(input) {
     const inputList = input.split('');
     let result = 0;
@@ -22,7 +23,14 @@ function convertStringToIntegers(input) {
     return result * prefix;
 }
 
-
+/*
+315 % 10 = 5 => add to result [5]
+(315 - 5) / 10 = 31
+31 % 10 = 1 => add to result [1, 5]
+(31 - 1) / 10 = 3
+3 % 10 = 3 => add to result [3, 1, 5]
+(3 - 3) / 10 = 0 => break
+ */
 function convertIntegersToString(input) {
     let result = [];
     let tempInput = Math.abs(input);
