3a1262259076456b5deaa51bf4a9ee3e623fe272
Regina Galieva <rg.galieva@gmail.com>
2018-10-22 08:25:42 -0700

    rename repeat file

---
+++ js/solutions/array/flattenArray/flattenArray.repeat.js
@@ -0,0 +1,15 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+
+function flattenArray(arr) {
+
+}
+
+function flattenArrayWithStack(arr) {
+
+}
+
+function flattenArrayWithQueue(arr) {
+
+}
+
+module.exports = {flattenArray, flattenArrayWithStack, flattenArrayWithQueue}
