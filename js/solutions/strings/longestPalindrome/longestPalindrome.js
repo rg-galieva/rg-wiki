18d22314bf673b15413826654b5323bf60e186b9
Regina Galieva <rg.galieva@gmail.com>
2018-11-17 13:23:50 -0800

    Strings: longest palindrome

--- js/solutions/strings/longestPalindrome/longestPalindrome.js
+++ js/solutions/strings/longestPalindrome/longestPalindrome.js
@@ -14,9 +14,36 @@ Output: "bb"
 */
 
 function longestPalindrome(input) {
-    const reversed = input.split('').reverse().join('');
+	if (!input || input.length <= 1) {
+		return input
+	}
 
+	let longest = input.slice(0, 1);
 
+	for (let i = 0; i < input.length; i++) {
+		let temp1 = expandAroundCenter(input, i, i);
+		let temp2 = expandAroundCenter(input, i, i + 1); // because center can be in between letters
+
+		let tempLongest = temp1.length >= temp2.length ? temp1 : temp2;
+
+		if (longest.length <= tempLongest.length) {
+			longest = tempLongest;
+		}
+	}
+
+	return longest;
+}
+
+function expandAroundCenter(input, left, right) {
+	let leftIndex = left;
+	let rightIndex = right;
+
+	while (leftIndex >= 0 && rightIndex < input.length && input.charAt(leftIndex) === input.charAt(rightIndex)) {
+		leftIndex--;
+		rightIndex++;
+	}
+
+	return input.slice(leftIndex + 1, rightIndex);
 }
 
 module.exports = longestPalindrome;
