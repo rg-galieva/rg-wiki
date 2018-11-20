85d912e135117b153ce3ee6bf499fe96146b768a
Regina Galieva <rg.galieva@gmail.com>
2018-11-19 19:56:25 -0800

    battleship attack

--- js/solutions/battleship/battleshipAttacks.js
+++ js/solutions/battleship/battleshipAttacks.js
@@ -2,22 +2,23 @@
   you attack a coordinate and add the result to your results array.
   you must have the result before you can make another guess
   battleshipAttacks can be of any number and you can't sink the battleship unless all attacks have been made
-*/
 
-function attackTargetInitial(target) {
+
+function attackTarget(target) {
 	setTimeout(() => {
 		console.log('ATTACKED', target);
 		return `hit ${target}`;
 	}, 1000);
 }
 
-function playBattleshipInitial(battleshipAttacks) {
+function playBattleship(battleshipAttacks) {
 	const results = [];
 	for (let i = 0; i < battleshipAttacks.length; i++) {
 		results.push(attackTargetInitial(battleshipAttacks[i]));
 	}
 	console.log('YOU SUNK MY BATTLESHIP', results)
 }
+*/
 
 const attackTarget = (target) => new Promise((resolve) => {
 	setTimeout(() => {
