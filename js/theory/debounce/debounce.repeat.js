f6982a8daf9d47a7477e585664b71e67f31ec13a
Regina Galieva <rg.galieva@gmail.com>
2018-11-27 14:37:35 -0800

    debounce repeat file

---
+++ js/theory/debounce/debounce.repeat.js
@@ -0,0 +1,13 @@
+/*
+Debouncing ensures that exactly one signal is sent for an event that may be happening several times.
+Throttling restricts the frequency of calls that a function receives to a fixed time interval.
+It is used to ensuring that the target function is not invoked more often than the specified delay.
+*/
+
+const debounce = (fn, delay) => {
+
+}
+
+const handleInputChange = (event) => {
+
+}
