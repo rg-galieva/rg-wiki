5277cc336af737defcb681cb6e04c44d9fe37ce2
Regina Galieva <rg.galieva@gmail.com>
2018-12-15 09:19:10 -0800

    maxSubArray repeat

--- js/solutions/array/maximumSubarray/maximumSubarray.repeat.js
+++ js/solutions/array/maximumSubarray/maximumSubarray.repeat.js
@@ -7,3 +7,15 @@ Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 */
+
+function maxSubArray(nums) {
+
+}
+
+
+function maxSubArrayList(nums) {
+
+}
+
+
+module.exports = {maxSubArray, maxSubArrayList};
